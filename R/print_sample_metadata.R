da7de4e83f4ff4c010f02c7eeae41b5b11b02d59
Zachary S.L. Foster <zacharyfoster1989@gmail.com>
2024-01-19 12:02:08 -0800

    Started metadata printing function as proof of concept
    Relates to issue #4

---
+++ R/print_sample_metadata.R
@@ -0,0 +1,29 @@
+
+#' Print a table of sample metadata
+#'
+#' Selects columns in the sample metadata to print and format the result for
+#' use as a static table in PDF or an interactive table in HTML.
+#'
+#' @param sample_data A tibble/data.frame with the sample metadata
+#' @param interactive Whether to use an HTML-based interactive format or not (default: TRUE)
+print_sample_metadata <- function(sample_data, interactive = TRUE) {
+
+  # Subset and reformat data for printing
+  column_key <- c(
+    sample = 'Sample ID',
+    fastq_1 = 'Forward Reads',
+    fastq_2 = 'Reverse Reads',
+    reference_id = 'Reference ID',
+    reference =  'Reference'
+  )
+  formatted_data <- sample_data[, names(column_key)]
+  colnames(formatted_data) <- column_key
+
+  # Print table
+  if (interactive) {
+    DT::datatable(formatted_data)
+  } else {
+    kableExtra::kbl(formatted_data, booktabs = TRUE)
+  }
+
+}
