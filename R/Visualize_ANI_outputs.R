57f83737c7b2b6bb22594e918b762cfb33c605d1
Martha Sudermann <masudermann@10-248-200-128.wireless.oregonstate.edu>
2024-01-22 16:55:32 -0800

    Update ANI fxn

--- R/Visualize_ANI_outputs.R
+++ R/Visualize_ANI_outputs.R
@@ -1,16 +1,25 @@
 #' Make an ANI heatmap and dendrogram.
 #'
 #' The ANI heatmap will either be static when PDF is rendered or interactive a NJ tree from Sourmash approximate ANI values
+#'
+#' @param sourmash_ani_matrix Approximate ANI matrix output from Sourmash analysis
+#' @param sample_data A tibble/data.frame with the sample metadata
+#' @param reference_data A tibble/data/frame with information on references used in analysis
+#' @param interactive Whether to use an HTML-based interactive format or not (default: TRUE)
+#' @return A heatmap and dendrogram
+#' @export
+#' @examples
+#' make_ani_heatmap(ani_matrix, ref_data, samp_data, interactive=FALSE)
 
-make_ani_heatmap <- function(ani_matrix, ref_data, samp_data, assembly_entries, interactive = TRUE) {
-  ani_matrix_format <-as.matrix(ani_matrix)
+make_ani_heatmap <- function(sourmash_ani_matrix, reference_data, sample_data, interactive = TRUE) {
+  ani_matrix_format <-as.matrix(sourmash_ani_matrix)
   colnames(ani_matrix_format) <- convert_id(colnames(ani_matrix_format))
   rownames(ani_matrix_format) <- convert_id(rownames(ani_matrix_format))
   assembly_entries <- rownames(ani_matrix_format)[grepl("_assembly$", rownames(ani_matrix_format))]
 
   name_key <- c(
-    setNames(ref_data$Organism, convert_id(ref_data$LastMajorReleaseAccession)),
-    setNames(samp_data$sample, convert_id(samp_data$sample)),
+    setNames(reference_data$Organism, convert_id(reference_data$LastMajorReleaseAccession)),
+    setNames(sample_data$sample, convert_id(sample_data$sample)),
     setNames(assembly_entries, assembly_entries)
   )
 
