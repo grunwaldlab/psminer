a516dcb445f474cabace967dbe435195c6c62a22
Martha Sudermann <masudermann@10-248-200-128.wireless.oregonstate.edu>
2024-01-22 17:49:26 -0800

    Test psminer

--- R/print_sample_metadata.R
+++ R/print_sample_metadata.R
@@ -7,7 +7,7 @@
 #' @param sample_data A tibble/data.frame with the sample metadata
 #' @param interactive Whether to use an HTML-based interactive format or not (default: TRUE)
 #' @export
-make_ani_nj_tree <- function(sample_data, interactive = TRUE) {
+print_sample_metadata <- function(sample_data, interactive = TRUE) {
 
   # Subset and reformat data for printing
   column_key <- c(
