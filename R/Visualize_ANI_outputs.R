6d2249213b777bd4308272edda8994d6d739f3d5
Martha Sudermann <masudermann@10-248-200-128.wireless.oregonstate.edu>
2024-01-23 14:45:33 -0800

    Start adding early phylogeny fxn

--- R/Visualize_ANI_outputs.R
+++ R/Visualize_ANI_outputs.R
@@ -37,7 +37,7 @@ make_ani_heatmap <- function(sourmash_ani_matrix, reference_data, sample_data, i
 
   if (interactive) {
     # Trying to find a good solution for an interactive plot, but based on complicated renaming of columns and rows,
-    # I am just putting this as a placeholder
+    # Still figuring this out. Defaulting to pheatmap for both for now.
     heatmap_ani <- pheatmap(ani_matrix_format, show_rownames = TRUE, labels_row = colnames(ani_matrix_format))
   } else {
     heatmap_ani <- pheatmap(ani_matrix_format, show_rownames = TRUE, labels_row = colnames(ani_matrix_format))
--- R/make_core_gene_phylogeny.R
+++ R/make_core_gene_phylogeny.R
@@ -2,13 +2,46 @@
 #'
 #' The tree will either be made using ggtree, if output is a PDF, or phylo
 #'
-#' @param
-#' @param
-#' @param
-#' @param
-#'
-#' @return
+#' @param core_phylo_path
+#' @return Core gene phylogeny
 #'
 #' @export
 #'
 #' @examples
+#'
+make_phylogeny <- function(core_phylogeny_path, sample_data,  reference_data, interactive = TRUE) {
+  #This helpfer function is used quite a bit, so perhaps it is worth pulling out of individual fxns?
+  convert_id <- function(ids) {
+    gsub(ids, pattern = "[.-]", replacement = "_")
+  }
+
+  core_tree <- ape::read.tree(core_phylogeny_path)
+
+  # Identify which tips are samples and references
+  sample_ids <- core_tree$tip.label[core_tree$tip.label %in% convert_id(sample_data$sample)]
+
+  # Root tree
+  colnames(ani_matrix) <- convert_id(colnames(ani_matrix))
+  rownames(ani_matrix) <- colnames(ani_matrix)
+  group_ani <- ani_matrix[rownames(ani_matrix) %in% core_tree$tip.label, colnames(ani_matrix) %in% core_tree$tip.label]
+  core_tree <- root(core_tree, names(which.min(colMeans(group_ani[sample_ids, ]))))
+
+  # Set tip labels to taxon names for reference sequences
+  # TODO-we need to generalize
+  name_key <- c(
+    reference_data$Organism,
+    sample_data$sample
+  )
+  names(name_key) <- c(
+    convert_id(reference_data$LastMajorReleaseAccession),
+    convert_id(sample_data$sample)
+  )
+  core_tree$tip.label <- name_key[core_tree$tip.label]
+
+
+  if (interactive) {
+
+  } else {
+
+  }
+}
