8b0ae21b2cf4e06a3e715de4576b73a649738a68
Martha Sudermann <masudermann@10-248-200-128.wireless.oregonstate.edu>
2024-01-22 17:46:35 -0800

    export fxn

--- R/print_sample_metadata.R
+++ R/print_sample_metadata.R
@@ -6,6 +6,7 @@
 #'
 #' @param sample_data A tibble/data.frame with the sample metadata
 #' @param interactive Whether to use an HTML-based interactive format or not (default: TRUE)
+#' @export
 make_ani_nj_tree <- function(sample_data, interactive = TRUE) {
 
   # Subset and reformat data for printing
