f8063320c554951c696e7e3c88fe58a722a05246
Martha Sudermann <masudermann@10-248-145-236.wireless.oregonstate.edu>
2024-01-30 12:59:47 -0800

    update function

--- R/parse_input_tables.R
+++ R/parse_input_tables.R
@@ -20,7 +20,7 @@ parse_sample_meta <- function(path, group) {
 #' Parse reference metadata files
 #'
 #'
-#' @param ref_data_path
+#' @param reference_data_path
 #' @param ref_ids_path
 #' @param sample_data
 #'
@@ -28,19 +28,19 @@ parse_sample_meta <- function(path, group) {
 #'
 #' @export
 
-parse_ref_meta <- function(ref_data_path, ref_ids_path, sample_data) {
+parse_ref_meta <- function(reference_data_path, ref_ids_path, sample_data) {
   convert_id <- function(ids) gsub(ids, pattern = "[.-]", replacement = "_")
 
-  ref_data <- read_tsv(ref_data_path , col_types = 'dcccccccccccccccddc')
+  reference_data <- read_tsv(reference_data_path , col_types = 'dcccccccccccccccddc')
   refs <- strsplit(read_lines(ref_ids_path), split = ';', fixed = TRUE)[[1]]
 
   reference_data$LastMajorReleaseAccession<-convert_id(reference_data$LastMajorReleaseAccession)
 
-  ref_data$origin = "refseq"
-  ref_data$reference_id = ref_data$LastMajorReleaseAccession
-  ref_data$display_name = ref_data$Organism
+  reference_data$origin = "refseq"
+  reference_data$reference_id = reference_data$LastMajorReleaseAccession
+  reference_data$display_name = reference_data$Organism
 
-  new_reference_ids <- unique(sample_data$reference_id[!is.na(sample_data$reference_id) & !sample_data$reference_id %in% ref_data$reference_id])
+  new_reference_ids <- unique(sample_data$reference_id[!is.na(sample_data$reference_id) & !sample_data$reference_id %in% reference_data$reference_id])
 
   if (length(new_reference_ids) > 0) {
     new_rows <- data.frame(
@@ -48,8 +48,8 @@ parse_ref_meta <- function(ref_data_path, ref_ids_path, sample_data) {
       reference_id = new_reference_ids,
       origin = "user"
     )
-    ref_data <- bind_rows(new_rows,ref_data)
+    reference_data <- bind_rows(new_rows,reference_data)
   }
 
-  return(ref_data)
+  return(reference_data)
 }
