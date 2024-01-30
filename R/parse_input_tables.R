a0de5414fa152eec65714fd955fee71687ca6f57
Martha Sudermann <masudermann@10-248-145-236.wireless.oregonstate.edu>
2024-01-30 11:49:33 -0800

    Update functions

--- R/parse_input_tables.R
+++ R/parse_input_tables.R
@@ -1,19 +1,19 @@
 #' Parse sample metadata files
 #'
 #'
-#' @param path the file path to 'samp_data.csv' file
+#' @param path the file path to 'sample_data.csv' file
 #' @param group parsed information on report group for specific set of samples
 #'
-#' @return samp_data dataframe
+#' @return sample_data dataframe
 #'
 #' @export
 
 parse_sample_meta <- function(path, group) {
-  all_samp_data <- read_csv(path, show_col_types = FALSE)
-  all_samp_data$modified_id <- gsub(all_samp_data$sample, pattern = "-", replacement = "_", fixed = TRUE) #TODO: check that this is needed with new data format
-  group_data <- strsplit(all_samp_data$report_group, split = ";")
-  samp_data <- all_samp_data[map_lgl(group_data, function(x) group %in% x), ]
-  return(samp_data)
+  all_sample_data <- read_csv(path, show_col_types = FALSE)
+  all_sample_data$modified_id <- gsub(all_sample_data$sample, pattern = "-", replacement = "_", fixed = TRUE) #TODO: check that this is needed with new data format
+  group_data <- strsplit(all_sample_data$report_group, split = ";")
+  sample_data <- all_sample_data[map_lgl(group_data, function(x) group %in% x), ]
+  return(sample_data)
 }
 
 
@@ -21,20 +21,20 @@ parse_sample_meta <- function(path, group) {
 #'
 #'
 #' @param refseq_path
-#' @param user_ref_path
+#' @param sample_data
 #'
 #' @return Reformatted reference metadata table containing both refseq-defined reference assemblies and user-defined assemblies
 #'
 #' @export
 
-parse_ref_meta <- function(refseq_path, user_ref_path) {
+parse_ref_meta <- function(refseq_path, sample_data) {
   ref_data <- read_tsv(ref_data_path , col_types = 'dcccccccccccccccddc')
   refs <- strsplit(read_lines(ref_ids_path), split = ';', fixed = TRUE)[[1]]
   ref_data$origin = "refseq"
   ref_data$reference_id = ref_data$LastMajorReleaseAccession
   ref_data$display_name = ref_data$Organism
 
-  new_reference_ids <- unique(samp_data$reference_id[!is.na(samp_data$reference_id) & !samp_data$reference_id %in% ref_data$reference_id])
+  new_reference_ids <- unique(sample_data$reference_id[!is.na(sample_data$reference_id) & !sample_data$reference_id %in% ref_data$reference_id])
 
   if (length(new_reference_ids) > 0) {
     new_rows <- data.frame(
