790cbd02ef4f7378be916c948d351dc2200cee51
cahuparo <paradarc@oregonstate.edu>
2024-02-12 17:22:15 -0800

    update yaml import function

--- R/parse_software_meta.R
+++ R/parse_software_meta.R
@@ -5,7 +5,7 @@
 #'
 #' @param version_path A character string specifying the path to the YAML file with version metadata.
 #' @return A tibble with columns `module`, `program`, and `version` detailing the software versions.
-#' @importFrom readr read_yaml
+#' @importFrom yaml read_yaml
 #' @importFrom tibble tibble
 #' @importFrom purrr map_chr
 #' @importFrom stringr str_split
@@ -14,7 +14,6 @@
 #' version_path <- file.path(params$inputs, "inputs", "versions.yml")
 #' version_data <- parse_software_meta(version_path)
 parse_software_meta <- function(version_path) {
-
   # Input validation
   if (!is.character(version_path) || length(version_path) != 1) {
     stop("version_path must be a single character string.")
@@ -26,19 +25,21 @@ parse_software_meta <- function(version_path) {
   }
 
   # Reading the YAML file
-  raw_version_data <- tryCatch(
-    {
-      unlist(readr::read_yaml(version_path))
-    },
-    error = function(e) {
-      stop("Failed to read YAML file: ", e$message)
-    }
-  )
+  raw_version_data <- tryCatch({
+    unlist(yaml::read_yaml(version_path))
+  },
+  error = function(e) {
+    stop("Failed to read YAML file: ", e$message)
+  })
 
   # Transforming the data into a tibble
   version_data <- tibble::tibble(
-    module = purrr::map_chr(stringr::str_split(names(raw_version_data), pattern = '\\.', n = 2), `[`, 1),
-    program = purrr::map_chr(stringr::str_split(names(raw_version_data), pattern = '\\.', n = 2), `[`, 2),
+    module = purrr::map_chr(stringr::str_split(
+      names(raw_version_data), pattern = '\\.', n = 2
+    ), `[`, 1),
+    program = purrr::map_chr(stringr::str_split(
+      names(raw_version_data), pattern = '\\.', n = 2
+    ), `[`, 2),
     version = unname(raw_version_data),
   )
 
