7317df9f2036c6d322feef5d72b6fc9e8e315efb
Zachary S.L. Foster <zacharyfoster1989@gmail.com>
2024-07-17 17:34:36 -0700

    phylogeny plotting mostly updated

--- R/plotters.R
+++ R/plotters.R
@@ -35,10 +35,13 @@ core_tree_plot <- function(input, collapse_by_tax = NULL, interactive = knitr::i
       sample_meta,
       ref_meta,
       color_by,
-      sendsketch # NOTE: A better source for the taxonomy should be found, perhaps based on reference NCBI taxon IDs.
+      sendsketch, # NOTE: A better source for the taxonomy should be found, perhaps based on reference NCBI taxon IDs.
+      interactive = interactive
     )
   })
+  names(tree_plots) <- color_by_cols
 
+  return(tree_plots)
 }
 
 
@@ -59,33 +62,45 @@ core_tree_plot <- function(input, collapse_by_tax = NULL, interactive = knitr::i
 #'
 #' @keywords internal
 plot_phylogeny <- function(trees, sample_meta, ref_meta, color_by = NULL, collapse_by_tax = NULL, interactive = knitr::is_html_output(), ...) {
-
-  # Subset taxonomy data to just the part used by each tree
-  ranks <- colnames(collapse_by_tax)[colnames(collapse_by_tax) != 'sample_id']
-  collapse_by_tax[ranks] <- lapply(collapse_by_tax[ranks], as.factor)
-  tree_tax <- dplyr::bind_rows(lapply(trees, function(tree) {
-    sample_ids <- tree$tip.label[tree$tip.label %in% sample_meta$sample_id]
-    tax_subset <- collapse_by_tax[collapse_by_tax$sample_id %in% sample_ids, colnames(collapse_by_tax) != 'sample_id']
-    tax_subset <- tax_subset[, apply(tax_subset, MARGIN = 2, function(col) length(unique(col)) == 1)]
-    unique(tax_subset)
-  }))
-  tree_tax <- tree_tax[, 1:ncol(tree_tax) < which(colnames(tree_tax) == 'g')]
-  keep_rank <- apply(tree_tax, MARGIN = 2, function(col) length(unique(col)) != 1)
-  keep_rank[1] <- TRUE # Always include the first rank
-  tree_tax <- tree_tax[, keep_rank]
-  tree_tax[] <- lapply(tree_tax, function(col) {
-    col <- as.character(col)
-    col[is.na(col)] <- 'test'
-    as.factor(col)
-  })
+  # If a single tree is provided, convert it to a list of a single tree to simplify code below
+  if (class(trees) == "phylo") {
+    trees <- list(trees)
+  }
 
   # Combine trees by connecting to the taxonomy-derived tree
-  base_tree <- ape::as.phylo(as.formula(paste0('~', paste0(colnames(tree_tax), collapse = '/'))), data = tree_tax)
-  mean_edge_len <- mean(unlist(lapply(trees, function(x) x$edge.length)))
-  base_tree$edge.length <- rep(mean_edge_len, nrow(base_tree$edge))
-  combined_tree <- base_tree
-  for (index in rev(seq_along(trees))) {
-    combined_tree <- ape::bind.tree(combined_tree, trees[[index]], where = index)
+  if (is.null(collapse_by_tax) || length(trees) == 1) {
+    if (length(trees) == 1) {
+      combined_tree <- trees[[1]]
+    } else {
+      stop(call. = FALSE, 'If "collapse_by_tax" is not provided, then only a single tree can be plotted at a time. Multiple trees were given.')
+    }
+  } else {
+    # Subset taxonomy data to just the part used by each tree
+    ranks <- colnames(collapse_by_tax)[colnames(collapse_by_tax) != 'sample_id']
+    collapse_by_tax[ranks] <- lapply(collapse_by_tax[ranks], as.factor)
+    tree_tax <- dplyr::bind_rows(lapply(trees, function(tree) {
+      sample_ids <- tree$tip.label[tree$tip.label %in% sample_meta$sample_id]
+      tax_subset <- collapse_by_tax[collapse_by_tax$sample_id %in% sample_ids, colnames(collapse_by_tax) != 'sample_id']
+      tax_subset <- tax_subset[, apply(tax_subset, MARGIN = 2, function(col) length(unique(col)) == 1)]
+      unique(tax_subset)
+    }))
+    tree_tax <- tree_tax[, 1:ncol(tree_tax) < which(colnames(tree_tax) == 'g')]
+    keep_rank <- apply(tree_tax, MARGIN = 2, function(col) length(unique(col)) != 1)
+    keep_rank[1] <- TRUE # Always include the first rank
+    tree_tax <- tree_tax[, keep_rank]
+    tree_tax[] <- lapply(tree_tax, function(col) {
+      col <- as.character(col)
+      col[is.na(col)] <- 'test'
+      as.factor(col)
+    })
+    # Combine trees
+    base_tree <- ape::as.phylo(as.formula(paste0('~', paste0(colnames(tree_tax), collapse = '/'))), data = tree_tax)
+    mean_edge_len <- mean(unlist(lapply(trees, function(x) x$edge.length)))
+    base_tree$edge.length <- rep(mean_edge_len, nrow(base_tree$edge))
+    combined_tree <- base_tree
+    for (index in rev(seq_along(trees))) {
+      combined_tree <- ape::bind.tree(combined_tree, trees[[index]], where = index)
+    }
   }
 
   # Prepare labels
@@ -112,41 +127,48 @@ plot_phylogeny <- function(trees, sample_meta, ref_meta, color_by = NULL, collap
   }
   tip_colors <- color_key[combined_tree$tip.label]
 
-
-
-
-  # plot
-  is_color <- function(x) {
-    unlist(lapply(x, function(y) {
-      tryCatch(is.matrix(col2rgb(y)), error = function(e) FALSE)
-    }))
-  }
-  if (! all(is_color(tip_colors))) {
-    factors <- unique(tip_colors)
-    factors <- factors[! is.na(factors)]
-    factor_key <- stats::setNames(viridis::viridis(length(factors)), factors)
-    tip_colors <- stats::setNames(factor_key[tip_colors], names(tip_colors))
-  }
-
-  browser()
-
-  # Convert characters that are not allowed in the format used by phylocanvas
-  tip_labels <- gsub(tip_labels, pattern = ')', replacement = ']', fixed = TRUE)
-  tip_labels <- gsub(tip_labels, pattern = '(', replacement = '[', fixed = TRUE)
-  tip_labels <- gsub(tip_labels, pattern = ':', replacement = '-', fixed = TRUE)
-  tip_labels <- gsub(tip_labels, pattern = ',', replacement = '.', fixed = TRUE)
-  tip_labels <- gsub(tip_labels, pattern = '_', replacement = ' ', fixed = TRUE)
-
-
-  combined_tree$tip.label <- tip_labels
-  names(tip_colors) <- tip_labels
-  phycanv <- phylocanvas(combined_tree, treetype = "rectangular", alignlabels = TRUE, showscalebar = TRUE, width = "100%", height = "10in")
-  phycanv$x$tree <- gsub(phycanv$x$tree, pattern = '_', replacement = ' ', fixed = TRUE) # Remove underscores from tip labels (phylocanvas converts spaces to underscores)
-
-  for (x in combined_tree$tip.label) { # NOTE: using the names instead of IDs like this could cause bugs if the name is not unique
-    print(x)
-    print( unname(tip_colors[x]))
-    phycanv <- style_node(phycanv, x, labelcolor = unname(tip_colors[x]), labeltextsize = 30)
+  # Plot tree
+  if (interactive) {
+    is_color <- function(x) {
+      unlist(lapply(x, function(y) {
+        tryCatch(is.matrix(col2rgb(y)), error = function(e) FALSE)
+      }))
+    }
+    if (! all(is_color(tip_colors))) {
+      factors <- unique(tip_colors)
+      factors <- factors[! is.na(factors)]
+      factor_key <- stats::setNames(viridis::viridis(length(factors), end = 0.8), factors)
+      tip_colors <- stats::setNames(factor_key[tip_colors], names(tip_colors))
+    }
+    # Convert characters that are not allowed in the format used by phylocanvas
+    tip_labels <- gsub(tip_labels, pattern = ',', replacement = '.', fixed = TRUE)
+    tip_labels <- gsub(tip_labels, pattern = '[():; ]+', replacement = '_')
+    tip_labels <- trimws(tip_labels, whitespace = '_')
+    combined_tree$tip.label <- tip_labels
+    names(tip_colors) <- tip_labels
+    phycanv <- phylocanvas(combined_tree, treetype = "rectangular", alignlabels = TRUE, showscalebar = TRUE, width = "100%", height = "10in")
+    # Add label colors
+    phycanv$x$nodestyles <- lapply(tip_labels, function(x) {
+      list(
+        highlighted = FALSE,
+        colour = "black",
+        shape = "circle",
+        size = 0.8,
+        leafStyle = list(strokeStyle = unname(tip_colors[x]), fillStyle = unname(tip_colors[x]), lineWidth = 1),
+        labelStyle = list(colour = unname(tip_colors[x]), textSize = 25, font = "Arial", format = "bold")
+      )
+    })
+    names(phycanv$x$nodestyles) <- unname(tip_labels)
+    # Remove underscores from tip labels (phylocanvas converts spaces to underscores)
+    phycanv$x$tree <- gsub(phycanv$x$tree, pattern = '_', replacement = ' ', fixed = TRUE)
+    names(phycanv$x$nodestyles) <- gsub(names(phycanv$x$nodestyles), pattern = '_', replacement = ' ', fixed = TRUE)
+    return(phycanv)
+  } else {
+    tree <- groupOTU(combined_tree, .node = combined_tree$tip.label[names(combined_tree$tip.label) %in% sample_meta$sample_id])
+    plotted_tree <- ggtree(tree) +
+      geom_tiplab(aes(color = group), show.legend = FALSE) +
+      scale_color_manual(values = rev(viridis::viridis(2, end = 0.8)), name = '')
+    return(plotted_tree)
   }
 }
 
