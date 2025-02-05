120c662121842b8c4c7ebec7191b0da021d60dd0
Zachary Foster <fosterz@pop-os.localdomain>
2025-02-05 15:59:37 -0800

    changes needed to allow for no or minimal references used

--- R/print_ani_table.R
+++ R/print_ani_table.R
@@ -15,16 +15,23 @@
 print_ani_table <- function(pairwise_matrix, sample_data, ref_data, interactive = knitr::is_html_output(), ...) {
   # Create table to print
   output <- do.call(rbind, lapply(sample_data$sample_id, function(id) { # loop over sample IDs and combine results into a table
-    ref_samp_comp <- pairwise_matrix[id, colnames(pairwise_matrix) %in% ref_data$ref_id]
+    ref_samp_comp <- pairwise_matrix[id, colnames(pairwise_matrix) %in% ref_data$ref_id, drop = FALSE]
     best_match <- which.max(ref_samp_comp)
-    next_best_match <-  which.max(ref_samp_comp[, -best_match])
+    if (ncol(ref_samp_comp) >= 2) {
+      next_best_match <- which.max(ref_samp_comp[, -best_match])
+      next_best_match_name <- ref_data$ref_name[ref_data$ref_id == names(next_best_match)]
+      next_best_match_ani <- format_number(unname(unlist(ref_samp_comp[next_best_match])))
+    } else {
+      next_best_match_name <- "NA (Too few references)"
+      next_best_match_ani <- "NA (Too few references)"
+    }
     data.frame(
       check.names = FALSE,
       'Sample' = sample_data$name[sample_data$sample_id == id],
       'Best match' =  ref_data$ref_name[ref_data$ref_id == names(best_match)],
       'ANI (%)' = format_number(unname(unlist(ref_samp_comp[best_match]))),
-      '2nd Best match' = ref_data$ref_name[ref_data$ref_id == names(next_best_match)],
-      '2nd ANI (%)' = format_number(unname(unlist(ref_samp_comp[next_best_match])))
+      '2nd Best match' = next_best_match_name,
+      '2nd ANI (%)' = next_best_match_ani
     )
   }))
   row.names(output) <- NULL
@@ -59,16 +66,26 @@ print_pocp_table <- function(pairwise_matrix, sample_data, ref_data, interactive
   # Create table to print
   sample_ids_in_matrix <- sample_data$sample_id[sample_data$sample_id %in% row.names(pairwise_matrix)]
   output <- do.call(rbind, lapply(sample_ids_in_matrix, function(id) { # loop over sample IDs and combine results into a table
-    ref_samp_comp_1 <- pairwise_matrix[id, colnames(pairwise_matrix) %in% ref_data$ref_id, drop = FALSE]
-    ref_samp_comp_2 <- t(pairwise_matrix[rownames(pairwise_matrix) %in% ref_data$ref_id, id, drop = FALSE])
-    ref_samp_comp_max <- mapply(max, unlist(ref_samp_comp_1), unlist(ref_samp_comp_2))
-    best_match <- which.max(ref_samp_comp_max)
+    if (any(colnames(pairwise_matrix) %in% ref_data$ref_id)) {
+      ref_samp_comp <- pairwise_matrix[id, colnames(pairwise_matrix) %in% ref_data$ref_id, drop = FALSE]
+      best_match <- which.max(ref_samp_comp)
+      best_ref_match_name <- ref_data$ref_name[ref_data$ref_id == names(best_match)]
+      best_ref_match_pocp <- format_number(unname(unlist(ref_samp_comp[best_match])))
+    } else {
+      best_ref_match_name <- "NA (No reference used)"
+      best_ref_match_pocp <- "NA (No reference used)"
+    }
+    samp_samp_comp <- pairwise_matrix[id, ! colnames(pairwise_matrix) %in% ref_data$ref_id & colnames(pairwise_matrix) != id, drop = FALSE]
+    best_match <- which.max(samp_samp_comp)
+    best_samp_match_name <- sample_data$name[sample_data$sample_id == names(best_match)]
+    best_samp_match_pocp <- format_number(unname(unlist(samp_samp_comp[best_match])))
     data.frame(
       check.names = FALSE,
       'Sample' = sample_data$name[sample_data$sample_id == id],
-      'Best match' =  ref_data$ref_name[ref_data$ref_id == names(best_match)],
-      'POCP 1 (%)' = format_number(unname(unlist(ref_samp_comp_1[best_match]))),
-      'POCP 2 (%)' = format_number(unname(unlist(ref_samp_comp_2[best_match])))
+      'Best reference match' =  best_ref_match_name,
+      'Reference POCP (%)' = best_ref_match_pocp,
+      'Best sample match' =  best_samp_match_name,
+      'Sample POCP (%)' = best_samp_match_pocp
     )
   }))
   row.names(output) <- NULL
